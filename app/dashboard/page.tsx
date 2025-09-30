dc3fbf924941221e3c92781a841aacc2f9b702ea
Sera <me@me@ciderboi.xyz>
2025-09-30 09:38:45 +0200

    fixed some style + [FEATURE] working forgot password

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,12 @@ import { pb } from "@/lib/db/pb";
 import { Player } from "@/lib/types";
 import { redirect } from "next/navigation";
 import { getServerAuth } from "@/lib/auth/server";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Dashboard - All India Forest Sports Meet 2025",
+};
 
 export default async function Dashboard() {
   // 🔑 Check auth on the server
